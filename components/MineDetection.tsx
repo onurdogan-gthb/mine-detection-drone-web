ad396ed69898b3edccace442594261da6b5ffcf5
synthonneur <crrccnt@pm.me>
2024-05-04 15:40:38 +0300

    fix

--- components/MineDetection.tsx
+++ components/MineDetection.tsx
@@ -1,37 +1,39 @@
 import React, { useState, useEffect } from "react";
+import Image from "next/image";
 import axios from "axios";
 
-import Image from "next/image";
 import radar from "../assets/radar.png";
 
 const MineDetection: React.FC = () => {
-  const [mine_detection, setMineDetection] = useState<boolean | null>(null);
+  const [mineDetection, setMineDetection] = useState<boolean | null>(null);
 
   useEffect(() => {
     const fetchMineDetection = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:5000/send-mine-detection", // URL
+          "http://localhost:5000/send-mine-detection",
         );
-        setMineDetection(response.data.mine_detection);
+
+        setMineDetection(response.data.mine_detected);
       } catch (error) {
         console.error("Error fetching mine detection: ", error);
       }
     };
 
-    const interval = setInterval(fetchMineDetection, 1000); // 1000 milliseconds = 1 second
+    const interval = setInterval(fetchMineDetection, 1000);
     fetchMineDetection();
 
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className="mr-4 my-8 p-14 shadow-md shadow-rust rounded bg-window">
+    <div className="my-8 mr-4 p-14 shadow-md shadow-rust rounded bg-window">
       <div className="flex justify-center">
         <Image width="64" height="64" alt="" src={radar} />
       </div>
-      <div className="flex justify-center items-center text-valid mt-8 px-8  text-2xl font-bold">
-        {mine_detection ? (
+
+      <div className="mt-8 px-8 flex justify-center items-center text-2xl font-bold">
+        {mineDetection ? (
           <div className="text-invalid">
             <p>Mine Found!</p>
           </div>
